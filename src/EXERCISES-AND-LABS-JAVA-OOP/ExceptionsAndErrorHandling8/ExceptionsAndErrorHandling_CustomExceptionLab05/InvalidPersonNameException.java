96f6bec25de393bc369dfeab31ea2a53f00867f4
Evelina Pancheva <81183303+evelinagp@users.noreply.github.com>
2023-03-26 17:22:37 +0300

    Delete src/EXERCISES-AND-LABS-JAVA-OOP/ExceptionsAndErrorHandling8 directory

--- src/EXERCISES-AND-LABS-JAVA-OOP/ExceptionsAndErrorHandling8/ExceptionsAndErrorHandling_CustomExceptionLab05/InvalidPersonNameException.java
+++
@@ -1,7 +0,0 @@
-package ExceptionsAndErrorHanling.ExceptionsAndErrorHandling_CustomExceptionLab05;
-
-public class InvalidPersonNameException extends RuntimeException {
-    public InvalidPersonNameException(String message) {
-        super(message);
-    }
-}
